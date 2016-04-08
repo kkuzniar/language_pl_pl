7c3c4e3a562283b5a99bd4b7c350e6924d62564d
Ubuntu <ubuntu@ip-10-0-8-59.eu-central-1.compute.internal>
2016-04-08 14:39:35 +0000

    commit

---
+++ registration.php
@@ -0,0 +1,8 @@
+
+<?php
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::LANGUAGE,
+    'magento2translations_pl_pl',
+    __DIR__
+);
+?>
